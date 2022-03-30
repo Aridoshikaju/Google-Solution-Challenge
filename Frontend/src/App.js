31f5bb3d86ac57a819147bb1d2a1a95943968c99
Aridoshikaju <surajpes23460@gmail.com>
2022-03-31 03:35:52 +0530

    Routes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -5,18 +5,39 @@ import Auth from './Components/Login'
 
 import './App.css';
 
+const storedData = JSON.parse(localStorage.getItem("userData"));
+let damm = false; //user does not exists
+if (storedData && storedData.token) {
+  damm = storedData.who;
+}
+
+
 function App() {
 
-  const [loggedin,setLoggedin] = useState()
+  const [whoLogged,setWhoLogged] = useState()
   const [token,setToken] = useState(false)
+  const storedData = JSON.parse(localStorage.getItem("userData"));
+  
   return (
-    <BrowserRouter>
-      <Routes>
-          <Route path="/api/auth" element={<Auth />} />
-          <Route path="/" element={<Landing />} >
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //       <Route path="/api/auth" element={<Auth />} />
+    //       <Route path="/" element={<Landing />} >
+    //     </Route>
+    //   </Routes>
+    // </BrowserRouter>
+<BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Landing />}/>
+        <Route path="provider"  element={"Provider"} />
+        <Route path="consumer" element={"Consumer"}/>
+          {/* <Route path=":teamId" element={<Team />} /> */}
+          <Route path="api/auth" element={<Auth />} />
+          {/* <Route path="" element={<LeagueStandings />} /> */}
+        {/* </Route> */}
+      {/* </Route> */}
+    </Routes>
+  </BrowserRouter>
   );
 }
 
